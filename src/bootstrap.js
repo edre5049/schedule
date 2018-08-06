3ee8b32e67059485879283d91ac2df134a8b04fd
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-06 11:29:16 -0600

    alot of stuff

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
- import Home from './components/home';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import Home from './components/home';
 import reducers from './reducers';
- const createStoreWithMiddleware = applyMiddleware()(createStore);
- import 'bootstrap/dist/css/bootstrap.css';
+
+const createStoreWithMiddleware = applyMiddleware()(compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore));
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
- function main() {
+
+function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <Home />
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
- document.addEventListener('DOMContentLoaded', main);
+
+document.addEventListener('DOMContentLoaded', main);
--- src/components/library.js/library.js
+++ src/components/library.js/library.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
- class Library extends Component {
-    render() {
-        return (
-            <div className="library">
-                <h1 className="library__title">Course Library</h1>
-            </div>
-        )
-    }
+
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as actions from "../../actions";
+
+import LibraryCourse from "./libraryCourse";
+
+class Library extends Component {
+  componentWillMount() {
+    this.props.fetchCourses();
+  }
+
+  renderCourses() {
+    const data = this.props.courses;
+
+    return data.map((course, index) => {
+      return <LibraryCourse {...course} key={index} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="library">
+        <div className="library__container">
+          <h1 className="library__container__title">Course Library</h1>
+          {this.renderCourses()}
+        </div>
+      </div>
+    );
+  }
 }
- export default Library; 
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps, actions)(Library);
